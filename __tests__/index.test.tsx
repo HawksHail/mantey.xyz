75db97ff7cc61b81c904b596258c047b8b268e72
Zion <zionman@verizon.net>
2021-12-22 16:20:27 -0600

    basic home page

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Home from "@/pages/index";
 
 describe("Home", () => {
-	it("renders a heading", () => {
+	it("renders name", () => {
 		render(<Home />);
 
 		const heading = screen.getByRole("heading", {
-			name: /welcome to next\.js!/i,
+			name: /zion mantey/i,
 		});
 
 		expect(heading).toBeInTheDocument();
