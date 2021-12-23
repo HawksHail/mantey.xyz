9f5e6a84a5094f0a5ab9fa5f5e4f965bcd1f343e
Zion <zionman@verizon.net>
2021-12-23 11:51:29 -0600

    Add theme context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
 
 import Layout from "@/components/Layout";
+import theme from "@/styles/theme";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ThemeProvider>
 	);
 }
 
---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+import { lightBlue } from "@mui/material/colors";
+
+export default createTheme({
+	palette: {
+		primary: {
+			main: lightBlue[500],
+		},
+	},
+});
