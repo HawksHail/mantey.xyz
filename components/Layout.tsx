f2ec581bea4f978fdf241d134a773fa8576b9e72
Zion <zionman@verizon.net>
2021-12-23 11:51:23 -0600

    Add navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,12 @@
 import type { ReactNode } from "react";
 
-// import Navbar from "./navbar";
+import Navbar from "@/components/Navbar";
 // import Footer from "./footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<>
-			{/* <Navbar /> */}
+			<Navbar />
 			<main>{children}</main>
 			{/* <Footer /> */}
 		</>
---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+const Navbar = () => {
+	return (
+		<AppBar>
+			<Toolbar>Zion</Toolbar>
+		</AppBar>
+	);
+};
+
+export default Navbar;
