99407b2dc324e93eb984d30c1af4aa309470f665
Zion <zionman@verizon.net>
2021-12-23 14:14:18 -0600

    Move viewport meta tag to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
 
+import Head from "next/head";
+
 import Layout from "@/components/Layout";
 import theme from "@/styles/theme";
 import { ThemeProvider } from "@mui/material/styles";
@@ -9,6 +11,12 @@ import { ThemeProvider } from "@mui/material/styles";
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={theme}>
+			<Head>
+				<meta
+					name="viewport"
+					content="initial-scale=1, width=device-width"
+				/>
+			</Head>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
