31baa931e721cfe5ae3e75156e07be7e99ca2edf
Zion <zionman@verizon.net>
2021-12-31 14:28:12 -0600

    Add darkmode toggle button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,18 @@
-import { AppBar, Toolbar } from "@mui/material";
+import Link from "@/components/Link";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+import { ColorModeToggleButton } from "./ColorThemeProvider";
 
 const Navbar = () => {
 	return (
-		<>
-			<AppBar>
-				<Toolbar>Zion</Toolbar>
-			</AppBar>
-			<Toolbar />
-		</>
+		<AppBar position="sticky" color="primary">
+			<Toolbar>
+				<Link href="/" color="inherit" underline="none">
+					Zion
+				</Link>
+				<ColorModeToggleButton />
+			</Toolbar>
+		</AppBar>
 	);
 };
 
