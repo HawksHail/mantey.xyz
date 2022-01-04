b975b6ad82dce57c2482b6ab8a864e1c5851903c
Zion <zionman@verizon.net>
2022-01-04 15:01:36 -0600

    basic contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+import { Typography } from "@mui/material";
+
+function contact() {
+	return (
+		<>
+			<Head>
+				<title>About | Zion Mantey</title>
+				<meta name="description" content="About Zion" />
+			</Head>
+			<Typography variant="h1">Contact Me</Typography>
+		</>
+	);
+}
+
+export default contact;
