c9e2aba81ec97b3d4657d471899325e464732a06
Zion <zionman@verizon.net>
2022-01-11 10:39:42 -0600

    Add links to contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,52 @@
 import Head from "next/head";
 
-import { Typography } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { Button, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function contact() {
 	return (
 		<>
 			<Head>
-				<title>About | Zion Mantey</title>
+				<title>Contact Me - Zion Mantey</title>
 				<meta name="description" content="About Zion" />
 			</Head>
-			<Typography variant="h1">Contact Me</Typography>
+			<Box
+				sx={{
+					textAlign: "center",
+					height: "80vh",
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+				}}
+			>
+				<Grid container spacing={2} justifyContent="center">
+					<Grid item xs={12}>
+						<Typography variant="h1">Contact Me</Typography>
+					</Grid>
+					<Grid item>
+						<Button
+							variant="contained"
+							color="primary"
+							startIcon={<EmailIcon />}
+							href="mailto:contact@mantey.xyz"
+						>
+							Email
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button
+							variant="contained"
+							color="primary"
+							startIcon={<TwitterIcon />}
+							href="https://twitter.com/messages/compose?recipient_id=399105671"
+						>
+							Twitter
+						</Button>
+					</Grid>
+				</Grid>
+			</Box>
 		</>
 	);
 }
