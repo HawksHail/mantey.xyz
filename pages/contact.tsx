ec7d3ce390c074587095ede6ed2601701bff4558
Zion <zionman@verizon.net>
2022-01-11 14:46:50 -0600

    simplify and adjust margin

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,7 +5,6 @@ import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import { Button, Grid, Typography } from "@mui/material";
-import { Box } from "@mui/system";
 
 function contact() {
 	return (
@@ -14,19 +13,30 @@ function contact() {
 				<title>Contact Me - Zion Mantey</title>
 				<meta name="description" content="Contact Zion" />
 			</Head>
-			<Box
+			<Grid
+				container
+				justifyContent="center"
+				alignContent="center"
+				spacing={2}
 				sx={{
 					textAlign: "center",
-					height: "80vh",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
 				}}
 			>
-				<Grid container spacing={2} justifyContent="center">
-					<Grid item xs={12}>
-						<Typography variant="h1">Contact Me</Typography>
-					</Grid>
+				<Grid
+					item
+					xs={12}
+					sx={{ marginTop: { xs: "2rem", sm: "6rem" } }}
+				>
+					<Typography variant="h1">Contact Me</Typography>
+				</Grid>
+				<Grid
+					container
+					item
+					spacing={2}
+					justifyContent="center"
+					xs={1}
+					sm="auto"
+				>
 					<Grid item>
 						<Button
 							variant="contained"
@@ -73,7 +83,7 @@ function contact() {
 						</Link>
 					</Grid>
 				</Grid>
-			</Box>
+			</Grid>
 		</>
 	);
 }
