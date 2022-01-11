bb168865642090b9b3c94c47c15806d20aa7b2e1
Zion <zionman@verizon.net>
2022-01-11 13:10:12 -0600

    adjust heading sizes

--- styles/theme.ts
+++ styles/theme.ts
@@ -16,8 +16,9 @@ export const getThemeOptions = (mode: PaletteMode): ThemeOptions => ({
 	},
 	typography: {
 		h1: { fontSize: "3.5rem" },
-		h2: { fontSize: "3rem" },
-		h3: { fontSize: "2.5rem" },
+		h2: { fontSize: "2.75rem" },
+		h3: { fontSize: "2.25rem" },
+		h4: { fontSize: "1.75rem" },
 	},
 });
 
